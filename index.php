d1d40b2d5349ce68d9d33df7642f039f4be2103e
jorgebp <jorge.beltranp@outlook.com>
2019-02-28 22:26:58 -0300

    Versión 1

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Inicio</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link rel="stylesheet" href="css/estilos.css">
+	<link rel="stylesheet" href="css/font-awesome.css">
+</head>
+<body>
+    <!--navbar -->
+    <ul class="navbar">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="servicios.php"> Servicios</a></li>
+        <li><a href="productos.php"> Productos</a></li>
+        <li><a href="galeria.php"> Galeria</a></li>
+        <li><a href="contacto.php"> Contacto</a></li>
+    </ul>
+        <!--slider -->
+        <div class="slideshow">
+		<ul class="slider">
+			<li class="liSlider">
+				<img src="img/1.jpg" alt="">
+				<section class="caption">
+					<h1>Sony</h1>
+					<p>Juegos exclusivos de la compañia.</p>
+				</section>
+			</li>
+			<li class="liSlider">
+				<img src="img/2.jpg" alt="">
+				<section class="caption">
+					<h1>Nintendo</h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci quis ipsa, id quidem quisquam unde.</p>
+				</section>
+			</li>
+			<li class="liSlider">
+				<img src="img/3.jpg" alt="">
+				<section class="caption">
+					<h1>PC Gaming</h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci quis ipsa, id quidem quisquam unde.</p>
+				</section>
+			</li >
+			<li class="liSlider">
+				<img src="img/4.jpg" alt="">
+				<section class="caption">
+					<h1>X-box</h1>
+					<p>Juegos para esta plataforma</p>
+				</section>
+			</li>
+		</ul>
+                
+		
+	
+		<div class="izq">
+			<span class="fa fa-chevron-left"></span>
+		</div>
+
+		<div class="der">
+			<span class="fa fa-chevron-right"></span>
+		</div>
+	</div>
+    <!--Reseña Empresa -->
+
+    <div class="wpb_text_column wpb_content_element  vc_custom_1433507959096">
+		<div class="wpb_wrapper">
+			<p></p>
+<h2 style="text-align: center;"></h2>
+<div style="text-align: center;">
+<div style="text-align: justify;">SNACK CENTER es parte de <strong>las soluciones</strong> no de los problemas, es por ello que presentamos máquinas nuevas de venta de snack y bebidas,&nbsp; que vienen con la última tecnología en expendio automático, además de máquinas de café y otros modernos modelos.&nbsp; Además nos encargamos de la reposición, mantención y arreglo de las máquinas.</div>
+<p>&nbsp;</p>
+<div style="text-align: justify;"></div>
+<div style="text-align: justify;"></div>
+<div style="text-align: left;">
+<p style="text-align: justify;">SNACKCENTER realiza <strong><span style="text-decoration: underline;">estudios personalizados</span></strong> para sus clientes, para buscar la mejor variedad de productos que requieran sus gustos y necesidades.</p>
+</div>
+</div>
+
+		</div> 
+	</div>
+
+</body>
+</html>
