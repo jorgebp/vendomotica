325b76d23d68a18917e43bf7dac20e977934d28c
jorgebp <jorge.beltranp@outlook.com>
2019-03-01 00:14:12 -0300

    Versión 2

--- index.php
+++ index.php
@@ -1,85 +1,241 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Inicio</title>
+	<meta charset="UTF-8">
+    <title>Trabajo Pagina Venta Juegos</title>
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-	<link rel="stylesheet" href="css/estilos.css">
-	<link rel="stylesheet" href="css/font-awesome.css">
+	<link rel="stylesheet" href="css/style.css">
+    <!--<link rel="stylesheet" href="css/font-awesome.css">-->
+    <style>
+		a { color: #2EA3F2; }
+
+		body { color: #666666; }
+
+		.et_pb_counter_amount, .et_pb_featured_table .et_pb_pricing_heading, .et_quote_content, .et_link_content, .et_audio_content { background-color: #2EA3F2; }
+
+		#main-header, #main-header .nav li ul, .et-search-form, #main-header .et_mobile_menu { background-color: #ffffff; }
+
+		#top-header, #et-secondary-nav li ul { background-color: #2EA3F2; }
+
+		.woocommerce a.button.alt, .woocommerce-page a.button.alt, .woocommerce button.button.alt, .woocommerce-page button.button.alt, .woocommerce input.button.alt, .woocommerce-page input.button.alt, .woocommerce #respond input#submit.alt, .woocommerce-page #respond input#submit.alt, .woocommerce #content input.button.alt, .woocommerce-page #content input.button.alt, .woocommerce a.button, .woocommerce-page a.button, .woocommerce button.button, .woocommerce-page button.button, .woocommerce input.button, .woocommerce-page input.button, .woocommerce #respond input#submit, .woocommerce-page #respond input#submit, .woocommerce #content input.button, .woocommerce-page #content input.button, .woocommerce-message, .woocommerce-error, .woocommerce-info { background: #2EA3F2 !important; }
+
+		#et_search_icon:hover, .mobile_menu_bar:before, .footer-widget h4, .et-social-icon a:hover, .comment-reply-link, .form-submit input, .et_pb_sum, .et_pb_pricing li a, .et_pb_pricing_table_button, .et_overlay:before, .entry-summary p.price ins, .woocommerce div.product span.price, .woocommerce-page div.product span.price, .woocommerce #content div.product span.price, .woocommerce-page #content div.product span.price, .woocommerce div.product p.price, .woocommerce-page div.product p.price, .woocommerce #content div.product p.price, .woocommerce-page #content div.product p.price, .et_pb_member_social_links a:hover { color: #2EA3F2 !important; }
+
+		.woocommerce .star-rating span:before, .woocommerce-page .star-rating span:before, .et_pb_widget li a:hover, .et_pb_bg_layout_light .et_pb_promo_button, .et_pb_bg_layout_light .et_pb_more_button, .et_pb_filterable_portfolio .et_pb_portfolio_filters li a.active, .et_pb_filterable_portfolio .et_pb_portofolio_pagination ul li a.active, .et_pb_gallery .et_pb_gallery_pagination ul li a.active, .wp-pagenavi span.current, .wp-pagenavi a:hover, .et_pb_contact_submit, .et_pb_bg_layout_light .et_pb_newsletter_button, .nav-single a, .posted_in a { color: #2EA3F2 !important; }
+
+		.et-search-form, .nav li ul, .et_mobile_menu, .footer-widget li:before, .et_pb_pricing li:before, blockquote { border-color: #2EA3F2; }
+
+		#main-footer { background-color: #000000; }
+
+		#top-menu a { color: #666666; }
+
+		#top-menu li.current-menu-ancestor > a, #top-menu li.current-menu-item > a, .bottom-nav li.current-menu-item > a { color: #2EA3F2; }
+
+		</style>
+<style type="text/css" id="et-custom-css">
+#top-header, #et-secondary-nav li ul {
+background-color: #001f61;
+}
+
+#top-menu li {
+display: inline-block;
+font-size: 19px;
+padding-right: 22px;
+}
+
+#logo {
+float: left;
+-moz-transition: all 0.2s ease-in-out;
+-webkit-transition: all 0.2s ease-in-out;
+transition: all 0.2s ease-in-out;
+margin-bottom: 18px;
+margin-top: 9px;
+}
+
+.main_title {
+margin-bottom: 20px;
+display: none;
+}
+
+#main-content .container {
+padding-top: 0px;
+}
+
+#main-header {
+line-height: 23px;
+font-weight: 500;
+top: 0;
+background-color: #fff;
+width: 100%;
+padding: 18px 0 0 0;
+min-height: 43px;
+box-shadow: 0 1px 0 rgba(0, 0, 0, 0);
+-moz-box-shadow: 0 1px 0 rgba(0, 0, 0, 0.1);
+-webkit-box-shadow: 0 0 0 rgba(0, 0, 0, 0.41);
+-moz-transition: all 0.2s ease-in-out;
+-webkit-transition: all 0.2s ease-in-out;
+transition: all 0.2s ease-in-out;
+position: relative;
+z-index: 999;
+}
+
+#footer-widgets {
+padding-top: 40px;
+}
+
+.footer-widget {
+float: left;
+width: 225px;
+margin: 0 60px 25px 0;
+color: #fff;
+}
+
+#main-footer {
+background-color: #000000;
+border-top: 10px solid #0063C5;
+}
+
+.wpb_content_element .wpb_accordion_wrapper .wpb_accordion_header, .wpb_content_element.wpb_tabs .wpb_tour_tabs_wrapper .wpb_tab {
+background-color: #C0C0C0;
+}
+</style>			
+							<style id="tt-easy-google-font-styles" type="text/css">
+			
+																						p {
+													}
+								
+																										h1 {
+													}
+								
+																										h2 {
+													}
+								
+																										h3 {
+													}
+								
+																										h4 {
+													}
+								
+																										h5 {
+													}
+								
+																										h6 {
+													}
+								
+										
+							</style>
+						<style type="text/css" data-type="vc_shortcodes-custom-css">.vc_custom_1433507959096{border-top-width: 1px !important;border-right-width: 1px !important;border-bottom-width: 1px !important;border-left-width: 1px !important;padding-top: 30px !important;padding-right: 25px !important;padding-bottom: 47px !important;padding-left: 25px !important;border-left-color: #009ac3 !important;border-left-style: solid !important;border-right-color: #009ac3 !important;border-right-style: solid !important;border-top-color: #009ac3 !important;border-top-style: solid !important;border-bottom-color: #009ac3 !important;border-bottom-style: solid !important;}</style>
 </head>
-<body>
-    <!--navbar -->
-    <ul class="navbar">
-        <li><a href="index.php">Home</a></li>
-        <li><a href="servicios.php"> Servicios</a></li>
-        <li><a href="productos.php"> Productos</a></li>
-        <li><a href="galeria.php"> Galeria</a></li>
-        <li><a href="contacto.php"> Contacto</a></li>
-    </ul>
-        <!--slider -->
-        <div class="slideshow">
-		<ul class="slider">
-			<li class="liSlider">
-				<img src="img/1.jpg" alt="">
-				<section class="caption">
-					<h1>Sony</h1>
-					<p>Juegos exclusivos de la compañia.</p>
-				</section>
-			</li>
-			<li class="liSlider">
-				<img src="img/2.jpg" alt="">
-				<section class="caption">
-					<h1>Nintendo</h1>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci quis ipsa, id quidem quisquam unde.</p>
-				</section>
-			</li>
-			<li class="liSlider">
-				<img src="img/3.jpg" alt="">
-				<section class="caption">
-					<h1>PC Gaming</h1>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci quis ipsa, id quidem quisquam unde.</p>
-				</section>
-			</li >
-			<li class="liSlider">
-				<img src="img/4.jpg" alt="">
-				<section class="caption">
-					<h1>X-box</h1>
-					<p>Juegos para esta plataforma</p>
-				</section>
-			</li>
-		</ul>
-                
+<body>  
+<div id="page-container">
+
+<div id="top-header" class="et_nav_text_color_light">
+<div class="container clearfix">
+
+
+    <div id="et-info">
+                        <span id="et-info-phone">+56 2 2449 1510</span>
+    
+    
+    <ul class="et-social-icons">
+
+
+</ul>				</div> <!-- #et-info -->
+
+
+    <div id="et-secondary-menu">
+    <div class="et_duplicate_social_icons">
+                    <ul class="et-social-icons">
+
+
+</ul>
+                </div>				</div> <!-- #et-secondary-menu -->
+
+</div> <!-- .container -->
+</div> <!-- #top-header -->
+
+    <!--navbar-->
+    <div class="et-top-navigation">
+        <nav class="et-top-navigation">
+            <ul id="top-menu" class="nav">
+            <li class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-2 current_page_item menu-item-35"><a href="index.php">Home</a></li>           
+            <li class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-2 current_page_item menu-item-35"><a href="registro.php">Servicio</a></li>      
+            <li class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-2 current_page_item menu-item-35"><a href="nosotros.php">Productos</a></li>    
+            <li class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-2 current_page_item menu-item-35"><a href="Noticia.php">Galeria</a></li>  
+            <li class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-2 current_page_item menu-item-35"><a href="Reseña.php">Contacto</a></li> 
+            </ul>
+        </nav>
+    </div>
+<!--slider-->
+	<div class="slideshow">
 		
-	
-		<div class="izq">
-			<span class="fa fa-chevron-left"></span>
-		</div>
-
-		<div class="der">
-			<span class="fa fa-chevron-right"></span>
-		</div>
 	</div>
-    <!--Reseña Empresa -->
+    <!--juegos destacados-->
+    <section>
+        <h1 class="text-uppercase text-center">BIENVENIDOS A NUESTRO SITIO WEB</h1>
+        <br />
 
-    <div class="wpb_text_column wpb_content_element  vc_custom_1433507959096">
-		<div class="wpb_wrapper">
-			<p></p>
-<h2 style="text-align: center;"></h2>
-<div style="text-align: center;">
-<div style="text-align: justify;">SNACK CENTER es parte de <strong>las soluciones</strong> no de los problemas, es por ello que presentamos máquinas nuevas de venta de snack y bebidas,&nbsp; que vienen con la última tecnología en expendio automático, además de máquinas de café y otros modernos modelos.&nbsp; Además nos encargamos de la reposición, mantención y arreglo de las máquinas.</div>
-<p>&nbsp;</p>
-<div style="text-align: justify;"></div>
-<div style="text-align: justify;"></div>
-<div style="text-align: left;">
-<p style="text-align: justify;">SNACKCENTER realiza <strong><span style="text-decoration: underline;">estudios personalizados</span></strong> para sus clientes, para buscar la mejor variedad de productos que requieran sus gustos y necesidades.</p>
-</div>
-</div>
+        <!--inicio container-->
+    <div class="container">
 
-		</div> 
-	</div>
+            <div>
+                <div class="wpb_wrapper">
+                    <div class="wpb_text_column wpb_content_element  vc_custom_1433507959096">
+                            <div class="text-align: justify;">
+                            VENDOMOTICA ALFONSO es parte de <strong>las soluciones</strong> no de los problemas, es por ello que presentamos máquinas nuevas de venta de snack y bebidas,  que vienen con la última tecnología en expendio automático, además de máquinas de café y otros modernos modelos.  Además nos encargamos de la reposición, mantención y arreglo de las máquinas.
+                            </div>
+                            <br>
+                            <div class="text-align: justify;">
+                            VENDOMOTICA ALFONSO realiza estudios personalizados para sus clientes, para buscar la mejor variedad de productos que requieran sus gustos y necesidades.
+                            </div>
+                        </div>
+                </div>
+        
 
-</body>
-</html>
+            
+                <div class="col-xs-4" >
+                            <div class="thumbnail">
+                                    <div class="text-align: justify;">
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea suscipit ex iure sequi! Optio consectetur, odio rem enim reprehenderit voluptatem incidunt itaque voluptate accusamus ea voluptatum nisi tenetur quos unde!
+                                    </div>
+                                </div>
+                </div>                
+    </div>
+    <!--Fin container-->
+                
+
+
+    
+                
+    </section>
+    <br /><br />
+    
+    <!--inicio de sesion-->
+    <button class="open-button" onclick="openForm()">Inicio de sesion</button>       
+        <div class="form-popup" id="myForm">
+          <form action="validar.php" class="form-container" method="post">
+            <h1>Login</h1>
+
+            <label for="email"><b>Email</b></label>
+            <input type="text" placeholder="Ingresar Email" name="email" required style="color:black">
+
+            <label for="psw"><b>Clave</b></label>
+            <input type="password" placeholder="Ingresar Clave" name="psw" required style="color:black">            
+
+            <button type="submit" class="btn">Login</button>
+            <button type="button" class="btn cancel" onclick="closeForm()">Close</button>
+          </form>
+        </div>
+        <script>
+            function openForm() {
+                document.getElementById("myForm").style.display = "block";
+            }
+
+            function closeForm() {
+                document.getElementById("myForm").style.display = "none";
+            }
+        </script>
+        <!-- fin del inicio de sesion-->
+        <script src="js/jquery-3.1.0.min.js"></script>
+        <script src="js/main.js"></script>
